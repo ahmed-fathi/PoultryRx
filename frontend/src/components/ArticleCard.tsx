ec1e127fc7007ae5d544f5e4c62c0a6ff1771c1a
Dr_el <dr_el@localhost>
2026-02-27 20:13:25 +0200

    Initial commit

--- frontend/src/components/ArticleCard.tsx
+++ frontend/src/components/ArticleCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import '../../styles/components/_article-card.scss'
+import '../styles/components/_article-card.scss'
 
 export default function ArticleCard({ article }: { article: any }) {
   return (
